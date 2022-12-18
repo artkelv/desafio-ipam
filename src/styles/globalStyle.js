b49234532f0685040d462f417cbc0f1d58f0b2bd
Arthut Kelvim <kelvimarthur@gmail.com>
2022-12-17 23:29:38 -0300

    style: adicao de fontes e globalstyle

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   body{
+    padding: 0;
+    margin: 0;
+
+    font-family: 'Inria Serif', serif;
+    font-family: 'Roboto', sans-serif;
+   } 
+`
